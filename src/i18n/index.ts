146c6843a6328c3633d6bbb3fd1d4c0f2b261fee
John William Bowen <johnwb1776@gmail.com>
2024-01-27 10:48:56 -0500

    added i18n class

---
+++ src/i18n/index.ts
@@ -0,0 +1,5 @@
+export { i18n } from './classes/i18n';
+
+export { LocaleBundle } from './classes/locale';
+
+export { Locale, fluentVariables } from './types';
---
+++ src/i18n/types.ts
@@ -0,0 +1,7 @@
+import { FluentVariable } from "@fluent/bundle";
+
+export type fluentVariables = Record<string, FluentVariable>;
+
+export { Locale, LocalizationMap } from 'discord.js';
+
+export const common = 'common';
