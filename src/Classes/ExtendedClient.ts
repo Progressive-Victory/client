cd0793811918b3b29d0b109e7a1cb138f8889000
Isabella <isabellafj97@gmail.com>
2024-03-04 17:10:02 -0600

    fix class typings maybe lazily

--- src/Classes/ExtendedClient.ts
+++ src/Classes/ExtendedClient.ts
@@ -263,3 +263,37 @@ export class ExtendedClient extends Client<true> {
 		return error instanceof Error;
 	}
 }
+
+declare module 'discord.js' {
+	interface BaseInteraction {
+		client: ExtendedClient;
+	}
+
+	interface Component {
+		client: ExtendedClient;
+	}
+
+	interface Message {
+		client: ExtendedClient;
+	}
+
+	interface BaseChannel {
+		client: ExtendedClient;
+	}
+
+	interface Role {
+		client: ExtendedClient;
+	}
+
+	interface Guild {
+		client: ExtendedClient;
+	}
+
+	interface User {
+		client: ExtendedClient;
+	}
+
+	interface GuildMember {
+		client: ExtendedClient;
+	}
+}
