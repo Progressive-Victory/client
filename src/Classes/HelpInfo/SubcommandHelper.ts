6823ff9d090be53cb6b53fdf6df1fde0e8f0c651
Isabella <isabellafj97@gmail.com>
2024-03-04 17:40:53 -0600

    Fix package file
    And run eslint again

--- src/Classes/HelpInfo/SubcommandHelper.ts
+++ src/Classes/HelpInfo/SubcommandHelper.ts
@@ -3,7 +3,7 @@ import { BaseHelper } from './BaseHelper';
 export class SubcommandHelper extends BaseHelper {
 	subcomands: BaseHelper[];
 
-	addSubcommand(helpInfo: BaseHelper| ((subcommandHelper: BaseHelper) => BaseHelper)) {
+	addSubcommand(helpInfo: BaseHelper | ((subcommandHelper: BaseHelper) => BaseHelper)) {
 		if (typeof helpInfo === 'function') this.subcomands.push(helpInfo(new BaseHelper()));
 		else this.subcomands.push(helpInfo);
 		return this;
