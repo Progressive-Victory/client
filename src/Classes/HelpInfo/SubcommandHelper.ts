c90b93f44addeb111a9a46b40f232e0193ff7e3b
John William Bowen <johnwb1776@gmail.com>
2024-03-04 17:48:21 -0500

    added functions to help builders

--- src/Classes/HelpInfo/SubcommandHelper.ts
+++ src/Classes/HelpInfo/SubcommandHelper.ts
@@ -3,8 +3,9 @@ import { BaseHelper } from './BaseHelper';
 export class SubcommandHelper extends BaseHelper {
 	subcomands: BaseHelper[];
 
-	addSubcommand(helpInfo: BaseHelper) {
-		this.subcomands.push(helpInfo);
+	addSubcommand(helpInfo: BaseHelper| ((subcommandHelper: BaseHelper) => BaseHelper)) {
+		if (typeof helpInfo === 'function') this.subcomands.push(helpInfo(new BaseHelper()));
+		else this.subcomands.push(helpInfo);
 		return this;
 	}
 }
